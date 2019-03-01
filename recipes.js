d00a8d952be8d269dc2ff8df03dd5440d7e9b9ef
Jesús <jeusxe@hotmail.com>
2019-03-01 11:59:21 +0100

    primera iteracion

--- recipes.js
+++ recipes.js
@@ -2,6 +2,47 @@ const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 const data = require('./data.js');
 
+const recipes = new Schema({
+  title: {
+    type: String,
+    require: true,
+    unique: true
+  },
+  level: {
+    type: String,
+    enum: ['Easy Peasy','Amateur Chef','UltraPro Chef']
+  },
+  ingredients: {
+    type: Array
+  },
+  cuisine:{
+    type: String,
+    require: true
+  },
+  dishType:{
+    type: String,
+    enum:['Breakfast','Dish','Snack','Drink','Dessert','Other']
+  },
+  image:{
+    type: String,
+    default: 'https://images.media-allrecipes.com/images/75131.jpg'
+  },
+  duration:{
+    type: Number,
+    minimum: 0
+  },
+  creator:{
+    type: String
+  },
+  created:{
+    type: Date,
+    default: Date.now
+  }
+})
+
+
+
+
 mongoose.connect('mongodb://localhost/recipeApp')
   .then(() => {
     console.log('Connected to Mongo!');
